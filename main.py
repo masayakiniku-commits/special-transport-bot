f5a003e7b48f194cd47e618512ae5a6dc34c5e7c
masayakiniku-commits <masayakiniku@gmail.com>
2026-03-26 17:01:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import os
 
 LINE_TOKEN = os.getenv("LINE_TOKEN")
 
-print("TOKEN確認:", LINE_TOKEN)  # ★追加
-
 def send_line(msg):
     res = requests.post(
         "https://notify-api.line.me/api/notify",
         headers={"Authorization": f"Bearer {LINE_TOKEN}"},
         data={"message": msg}
     )
-    print("STATUS:", res.status_code)  # ★追加
-    print("BODY:", res.text)          # ★追加
+    print("STATUS:", res.status_code)
+    print("BODY:", res.text)
 
-send_line("テスト通知")
+# ★絶対に通る確認用
+print("ここまで来てる")
+
+send_line("🔥 強制通知テスト")
