4907d5e1a297d996af2e67fb8cf5f857df134510
masayakiniku-commits <masayakiniku@gmail.com>
2026-03-28 00:42:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,47 +5,11 @@ BEARER_TOKEN = os.getenv("X_BEARER_TOKEN")
 LINE_TOKEN = os.getenv("LINE_TOKEN")
 
 def send_line(msg):
-requests.post(
-"https://notify-api.line.me/api/notify",
-headers={"Authorization": "Bearer " + LINE_TOKEN},
-data={"message": msg}
-)
-
-def get_koshu_tweets():
-url = "https://api.twitter.com/2/tweets/search/recent"
-
-```
-params = {
-    "query": "(甲種輸送 OR 配給輸送) (EF210 OR EF65 OR DE10 OR DD200) -is:retweet",
-    "max_results": 10
-}
-
-headers = {
-    "Authorization": "Bearer " + BEARER_TOKEN
-}
-
-res = requests.get(url, headers=headers, params=params)
-data = res.json()
-
-return data.get("data", [])
-```
+print("LINE送信テスト")
+return
 
 def main():
-tweets = get_koshu_tweets()
-
-```
-if not tweets:
-    print("取得なし")
-    return
-
-for t in tweets:
-    text = t["text"]
-
-    if "甲種輸送" in text:
-        msg = "🚃甲種輸送検知\n\n" + text
-        send_line(msg)
-        print("送信:", text)
-```
+print("起動OK")
 
 if **name** == "**main**":
 main()
