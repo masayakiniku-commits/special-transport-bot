144fd469927b0435089e360e2d6d00a23eb63acb
masayakiniku-commits <masayakiniku@gmail.com>
2026-03-28 00:44:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 import os
-import requests
-
-BEARER_TOKEN = os.getenv("X_BEARER_TOKEN")
-LINE_TOKEN = os.getenv("LINE_TOKEN")
-
-def send_line(msg):
-print("LINE送信テスト")
-return
 
 def main():
-print("起動OK")
+    print("OK")
 
-if **name** == "**main**":
-main()
+if __name__ == "__main__":
+    main()
